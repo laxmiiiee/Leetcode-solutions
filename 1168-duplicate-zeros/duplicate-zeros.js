e826773ea4c7cbb5be72d7ea02619c2d397f99d4
Lakshmi priya <laxmiipriyak@gmail.com>
2026-09-01 17:05:48 +0530

    Time: 13 ms (13.72%) | Memory: 54.6 MB (96.39%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.js
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -2,17 +2,18 @@
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
-var duplicateZeros = function(arr) {
-    let p = 0;
-    while (p < arr.length){
-        if (arr[p] === 0){
-            arr.splice(p+1,0,0);
-            arr.pop()
-            p += 2
-        }else{
-            p++
+var duplicateZeros = function (arr) {
+    let l = 0;
+    while (l < arr.length) {
+        if (arr[l] === 0) {
+            for (let r = arr.length - 1; r > l; r--) {
+                arr[r] = arr[r - 1];
+            }
+            l += 2
+        } else {
+            l++
         }
-       
+
     }
     return arr
 };
